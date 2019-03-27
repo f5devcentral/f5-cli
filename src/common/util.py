30fe1d99375bc6048c3c70a802d506bc50ef6646
James Sevedge <j.sevedge@f5.com>
2019-03-26 17:48:24 -0700

    start build out

---
+++ src/common/util.py
@@ -0,0 +1,5 @@
+# Helper utility functions
+import datetime
+
+def multiply(n1, n2):
+    return n1 * n2
---
+++ tests/test_util.py
@@ -0,0 +1,9 @@
+# import packages
+import datetime
+import os, sys
+sys.path.append(os.getcwd()) # TODO: fix this import process
+
+from src.common import util # pylint: disable=import-error
+
+def test_multiply():
+    assert util.multiply(2, 5) == 10
