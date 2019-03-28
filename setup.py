abb510627b37f4736f568b9b031e96e251455bcd
shimkus <m.shimkus@f5.com>
2019-03-28 10:17:18 -0700

    added setup entry points

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
 setup(name='f5cloudcli',
-      version='0.9.0',
-      description='F5 CLoud CLI',
+      version='0.1.2',
+      description='F5 Cloud CLI',
       url='http://github.com/F5Networks/f5-cloud-cli',
       author='F5 Ecosystems Group',
       author_email='solutionsfeedback@f5.com',
       license='MIT',
       packages=['f5cloudcli', 'f5cloudcli.commands', 'f5cloudcli.shared'],
+      entry_points={
+          'console_scripts': [
+              'f5cloudcli = f5cloudcli.cli:cli'
+          ]
+      },
       zip_safe=False)
