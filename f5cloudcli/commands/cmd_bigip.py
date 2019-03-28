fe601506beb9a55bfbe287b661f537028083272c
shimkus <m.shimkus@f5.com>
2019-03-28 11:00:14 -0700

    added toolchain and configure

--- f5cloudcli/commands/cmd_bigip.py
+++ f5cloudcli/commands/cmd_bigip.py
@@ -12,3 +12,10 @@ def cli(ctx):
 @pass_context
 def login(ctx, user, password):
     ctx.log('Logging in to BIG-IP... user:%s password:%s', user, password)
+
+@cli.command()
+@click.option('--cloud', help='Public cloud environment where the tagged BIG-IPs are running', required=True)
+@click.option('--tag', help='Tag (key/value pair) for discovery', required=True)
+@pass_context
+def discover(ctx, cloud, tag):
+    ctx.log('Discovering BIG-IPs.. cloud:%s tag:%s', cloud, tag)
--- f5cloudcli/commands/cmd_cloud_services.py
+++ f5cloudcli/commands/cmd_cloud_services.py
@@ -6,3 +6,15 @@ from f5cloudcli.cli import pass_context
 def cli(ctx):
     """Cloud Services CLI sub-command"""
     pass
+
+@cli.command()
+@click.option('--destination', help='Toolchain destination BIG-IP', required=True)
+@pass_context
+def tool(ctx, destination):
+    ctx.log('Toolchain destination BIG-IP... destination:%s', destination)
+
+@cli.command()
+@click.option('--destination', help='Configuration destination BIG-IP', required=True)
+@pass_context
+def configure(ctx, destination):
+    ctx.log('Configuration destination BIG-IP... destination:%s', destination)
