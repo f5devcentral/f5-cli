065d29a3d2342a6cd69dc47805a94c0f1c5022a3
shimkus <m.shimkus@f5.com>
2019-04-04 14:11:00 -0700

    this is terrible I apologize

--- f5cloudcli/commands/cmd_bigip.py
+++ f5cloudcli/commands/cmd_bigip.py
@@ -8,19 +8,34 @@ def cli(ctx): # pylint: disable=unused-argument
     """ Click cli function """
     # pass
 
+USER_HELP= 'Username for logging into the BIG-IP'
+SSH_KEY_HELP= 'SSH key for logging into the BIG-IP'
 @cli.command()
-@click.option('--user', help='Username for the BIG-IP', required=True, prompt=True)
-@click.password_option('--password', help='Password for the BIG-IP', required=True, prompt=True)
+@click.option('--user', help=USER_HELP, required=True, prompt=False)
+@click.argument('private-key', required=False, type=click.File('sshkey'))
 @PASS_CONTEXT
-def login(ctx, user, password):
+def login(ctx, user, private_key):
     """ Click cli command """
-    ctx.log('Logging in to BIG-IP as %s', user, password)
+    ctx.log('Logging in to BIG-IP as %s', user)
 
 CLOUD_HELP = 'Public cloud environment where the tagged BIG-IPs are running'
+TAG_HELP = 'Tag (key/value pair) for discovery'
 @cli.command()
-@click.option('--cloud', help=CLOUD_HELP, required=True, prompt=True)
-@click.option('--tag', help='Tag (key/value pair) for discovery', required=True, prompt=True)
+@click.option('--provider', help=CLOUD_HELP, required=True, prompt=True, type=click.Choice(['aws', 'azure', 'gcp']))
+@click.option('--tag', help=TAG_HELP, required=True, prompt=True)
 @PASS_CONTEXT
-def discover(ctx, cloud, tag):
+def discover(ctx, provider, tag):
     """ Click cli command """
-    ctx.log('Discovering all BIG-IPs in %s with tag %s', cloud, tag)
+    ctx.log('Discovering all BIG-IPs in %s with tag %s', provider, tag)
+
+TOOLCHAIN_HELP = 'Use this command to install, upgrade, and verify Automation Toolchain packages and services'
+@cli.command(help=TOOLCHAIN_HELP)
+@click.argument('component', required=False, type=click.Choice(['do', 'as3', 'ts', 'failover']))
+@click.argument('context', required=False, type=click.Choice(['package', 'service']))
+@click.argument('action', required=False, type=click.Choice(['install', 'upgrade', 'verify', 'remove', 'create']))
+@click.option('-v', '--version', required=False, prompt=False)
+@click.argument('declaration', required=False, type=click.File('decl'))
+@PASS_CONTEXT
+def toolchain(ctx, component, context, action, version, declaration):
+    """ Click cli command """
+    ctx.log('%s %s %s %s %s', action, component, context, version, declaration)
---
+++ f5cloudcli/commands/cmd_provider.py
@@ -0,0 +1,21 @@
+""" This file provides the 'provider' implementation of the CLI. """
+import click
+from f5cloudcli.cli import PASS_CONTEXT, AliasedGroup
+
+@click.group('provider', short_help='Provider', cls=AliasedGroup)
+@PASS_CONTEXT
+def cli(ctx): # pylint: disable=unused-argument
+    """ Provider sub-command """
+    # pass
+
+LOGIN_HELP='Use this command to log into the cloud provider'
+USER_HELP= 'Username for logging into the provider'
+PASSWORD_HELP= 'Password for logging into the provider'
+@cli.command(help=LOGIN_HELP)
+@click.argument('environment')
+@click.option('--user', help=USER_HELP, required=True, prompt=True)
+@click.password_option('--password', help=PASSWORD_HELP, required=True, prompt=True)
+@PASS_CONTEXT
+def login(ctx, environment, user, password):
+    """ Click cli command """
+    ctx.log('Logging into %s as %s', environment, user)
