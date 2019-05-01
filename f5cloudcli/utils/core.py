bd2e125eeb3a27f4dfb3c0dbc8fc80cd674cb53e
James Sevedge <j.sevedge@f5.com>
2019-05-01 14:37:16 -0700

    add prototype support for toolchain service

--- f5cloudcli/utils/core.py
+++ f5cloudcli/utils/core.py
@@ -1,5 +1,11 @@
 """ Core utility functions """
 
+import os
+
 def multiply(n_1, n_2):
     """ Multiply two numbers """
     return n_1 * n_2
+
+def convert_to_absolute(file):
+    """Convert file to absolute path """
+    return os.path.abspath(os.path.join(os.getcwd(), file))
