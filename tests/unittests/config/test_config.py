0432267767ce3021f9b865cc4a5d85e6dd6e0280
Garrett Dieckmann <g.dieckmann@f5.com>
2019-08-15 14:19:05 -0700

    add config test to ensure auth data is merged

--- tests/unittests/config/test_config.py
+++ tests/unittests/config/test_config.py
@@ -9,6 +9,7 @@ from f5cloudcli import constants
 
 class TestConfigClient(object):
     """ Test Class: configure client """
+
     def test_read_exist_auth(self, mocker):
         """ Read an existing Auth file
         Given
@@ -77,7 +78,6 @@ class TestConfigClient(object):
             client.store_auth()
             mock_json_dump.assert_called_once()
 
-    '''
     def test_write_to_existing_auth_file(self, mocker):
         """ Write credentials to an existing Auth file
         Given
@@ -91,7 +91,31 @@ class TestConfigClient(object):
         - The existing Auth file is read
         - The Auth file is overwritten, containing merged credentials
         """
-    '''
+        mock_path_exist = mocker.patch("f5cloudcli.config.os.path.exists")
+        mock_path_exist.return_value = True
+        bigip_auth = {
+            'username': 'root',
+            'password': '123',
+            'host': '1.2.3.4'
+        }
+        client = ConfigClient(
+            group_name=constants.BIGIP_GROUP_NAME,
+            auth=bigip_auth)
+        mock_path_isfile = mocker.patch("f5cloudcli.config.os.path.isfile")
+        mock_path_isfile.return_value = True
+        mock_json_load = mocker.patch("f5cloudcli.config.json.load")
+        cloud_services_auth = {'username': 'me@home.com', 'password': '123'}
+        mock_json_load.return_value = {
+            'CLOUD_SERVICES': cloud_services_auth
+        }
+        mock_json_dump = mocker.patch("f5cloudcli.config.json.dump")
+        with mocker.patch('f5cloudcli.config.open', new_callable=mocker.mock_open()):
+            client.store_auth()
+            mock_json_dump.assert_called_once()
+        x = mock_json_dump
+        mock_json_dump_wrote = mock_json_dump.call_args_list[0][0][0]
+        assert mock_json_dump_wrote[constants.BIGIP_GROUP_NAME] == bigip_auth
+        assert mock_json_dump_wrote[constants.CLOUD_SERVICES_GROUP_NAME] == cloud_services_auth
 
     def test_write_nonexist_config_directory(self, mocker):
         """ Write Auth file into a non-exist directory
@@ -109,14 +133,14 @@ class TestConfigClient(object):
         mock_path_exist = mocker.patch("f5cloudcli.config.os.path.exists")
         mock_path_exist.return_value = False
         mock_make_dir = mocker.patch("f5cloudcli.config.os.makedirs")
-        auth={
-            'username': 'me@f5.com',
+        auth = {
+            'username': 'me@home.com',
             'password': '1234'
         }
         client = ConfigClient(
             group_name=constants.CLOUD_SERVICES_GROUP_NAME,
             auth=auth
-            )
+        )
         mock_path_is_file = mocker.patch("f5cloudcli.config.os.path.isfile")
         mock_path_is_file.return_value = False
         mock_json_dump = mocker.patch("f5cloudcli.config.json.dump")
