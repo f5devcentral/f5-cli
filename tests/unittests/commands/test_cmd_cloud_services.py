2b3373607e902c6d5e58d3b723c64eb3282745f6
Garrett Dieckmann <g.dieckmann@f5.com>
2019-08-19 13:38:26 -0700

    add unit tests for subscription.show()

--- tests/unittests/commands/test_cmd_cloud_services.py
+++ tests/unittests/commands/test_cmd_cloud_services.py
@@ -1,6 +1,10 @@
 import pytest
+import click
+
 from click.testing import CliRunner
 
+from f5cloudsdk.cloud_services import ManagementClient
+from f5cloudsdk.cloud_services.subscription import SubscriptionClient
 from f5cloudcli.config import ConfigClient
 
 # Module under test
@@ -10,6 +14,12 @@ from f5cloudcli import constants
 # Test Constants
 TEST_USER = 'TEST USER'
 TEST_PASSWORD = 'TEST PASSWORD'
+SUBSCRIPTION_ID = 's-123'
+
+MOCK_CONFIG_CLIENT_READ_AUTH_RETURN_VALUE = {
+    'username': 'test_user',
+    'password': 'test_password'
+}
 
 
 class TestCommandBigIp(object):
@@ -37,6 +47,28 @@ class TestCommandBigIp(object):
         mock_config_client_store_auth = mocker.patch.object(
             ConfigClient, "store_auth")
         return mock_config_client_store_auth
+
+    @pytest.fixture
+    def config_client_read_auth_fixture(self, mocker):
+        """ PyTest fixture mocking ConfigClient's read_auth method """
+        mock_config_client_read_auth = mocker.patch.object(
+            ConfigClient, "read_auth")
+        mock_config_client_read_auth.return_value = MOCK_CONFIG_CLIENT_READ_AUTH_RETURN_VALUE
+        return mock_config_client_read_auth
+
+    @pytest.fixture
+    def mgmt_client_fixture(self, mocker):
+        """ PyTest fixture returning mocked Cloud Services Management Client """
+        mock_management_client = mocker.patch.object(ManagementClient, '__init__')
+        mock_management_client.return_value = None
+        return mock_management_client
+
+    @pytest.fixture
+    def subscription_client_fixture(self, mocker):
+        """ PyTest fixture returning mocked Cloud Services Subscription Client """
+        mock_subscription_client = mocker.patch.object(SubscriptionClient, '__init__')
+        mock_subscription_client.return_value = None
+        return mock_subscription_client
 
     def test_dns_service(self):
         """ Test DNS service
@@ -118,8 +150,11 @@ class TestCommandBigIp(object):
         }
         assert result.output == f"Configuring F5 Cloud Services Auth for {TEST_USER} with ******\n"
 
-
-    def test_cmd_cloud_services_subscription_show(self, mocker):
+    def test_cmd_cloud_services_subscription_show(self,
+                                                  mocker,
+                                                  config_client_read_auth_fixture,  # pylint: disable=unused-argument
+                                                  mgmt_client_fixture,  # pylint: disable=unused-argument
+                                                  subscription_client_fixture):  # pylint: disable=unused-argument
         """ Execute a 'show' action against an F5 Cloud Services subscription
 
         Given
@@ -131,7 +166,38 @@ class TestCommandBigIp(object):
 
         Then
         - Authentication data is read from disk
-        - A Management client is created
+        - A Management client is createddd
         - A Subscription client is created
         - The show command is executed
         """
+        mock_subscription_client_show = mocker.patch.object(
+            SubscriptionClient, "show")
+        mock_subscription_client_show.return_value = {
+            'subscription_id': SUBSCRIPTION_ID,
+            'account_id': 'a-123'
+        }
+        click_echo = mocker.patch.object(click, 'echo')
+        self.runner.invoke(cli, ['subscription', 'show',
+                                 '--subscription-id', SUBSCRIPTION_ID])
+
+        expected_response = '{"subscription_id": "s-123", "account_id": "a-123"}'
+        assert click_echo.call_args[1]['message'] == expected_response
+
+    def test_cmd_cloud_services_subscription_bad_action(self):
+        """ Execute an unimplemented action for F5 Cloud Services subscription
+
+        Given
+        - Cloud Services is available, and end-user has an account
+        - The user has already configured authentication with Cloud Services
+
+        When
+        - User executes a 'blahblah' command against F5 Cloud Services
+
+        Then
+        - The CLI responds that an unimplemented action was executed
+        """
+        bad_action = 'blahblah'
+
+        result = self.runner.invoke(cli, ['subscription', bad_action])
+        assert f"invalid choice: {bad_action}" in result.output
+        assert result.exception
