0d822d21ae1ec67e3df7e5585e2ba5ef0a45194c
Garrett Dieckmann <g.dieckmann@f5.com>
2019-08-22 12:47:41 -0700

    move all examples into code files

--- docs/examples/bigip.rst
+++ docs/examples/bigip.rst
@@ -1,4 +1,4 @@
 :mod:`f5 bigip commands examples`
-====================================
+=================================
 
 .. automodule:: f5cloudcli.commands.cmd_bigip
--- f5cloudcli/commands/cmd_bigip/__init__.py
+++ f5cloudcli/commands/cmd_bigip/__init__.py
@@ -1,6 +1,9 @@
-"""This file provides the ‘bigip’ implementation of the CLI.
+# pylint: disable=line-too-long
+"""Below are examples of using the Cloud CLI to interact with a BIG-IP.
 
-    Example - Discover BIG-IPs running in a Cloud Provider:: none
+    1. Discover BIG-IPs running in a Cloud Provider
+    ------------------------------------------------
+    The following is an example of how to discover information about a BIG-IP, including IP addresses, running in a cloud provider. ::
 
         $ export F5_CLI_PROVIDER_ACCESS_KEY=<aws_access_key_id>
         $ export F5_CLI_PROVIDER_SECRET_KEY=<aws_secret_access_key>
@@ -12,17 +15,37 @@
             ...
         }
 
-    Example - Configure authentication to a BIG-IP:: none
+    2. Configure authentication to a BIG-IP
+    ---------------------------------------
+    The following is an example of how to configure authentication for a BIG-IP. Any commands that interact with a BIG-IP require that authentication to that BIG-IP is already configured. ::
 
         $ f5 bigip configure-auth --host 54.224.182.104 --user myuser
         Password:
         Configuring BIG-IP Auth to 54.224.182.104 as myuser with ******
 
-    Example - Install an Automation Toolchain package:: none
+
+    3. Install an Automation Toolchain package
+    ------------------------------------------
+    The following is an example of how to install the Declarative Onboarding package onto a BIG-IP. ::
 
         $ f5 bigip toolchain package install --component do
         Toolchain component package do installed
+
+
+    4. Install an Automation Toolchain service
+    ------------------------------------------
+    The following is an example of how to configure a new service using AS3 ::
+
+        $ f5 bigip toolchain service --component as3 --declaration as3_decl.json create
+        Toolchain component service create: {
+            "declaration": {
+                ...
+            }
+        }
+
+
 """
+# pylint: enable=line-too-long
 
 import os
 
